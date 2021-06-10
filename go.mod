c70421e884ab3ad3cb125d1b6d76ac358280114a
Matt Baer <matt@write.as>
2021-06-10 17:24:50 -0400

    Add image decoding with auto-orientation and ICC profile extraction

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/snapas/img
 
 go 1.13
+
+require (
+	github.com/disintegration/imageorient v0.0.0-20180920195336-8147d86e83ec // indirect
+	github.com/snapas/imageorient v0.0.0-20200811211423-9f66c3153654
+)
---
+++ image.go
@@ -0,0 +1,51 @@
+// Package img enables basic image operations with automatic features like orientation correction
+// and EXIF / ICC profile preservation, so you can transform photos without losing important
+// metadata.
+package img
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/snapas/imageorient"
+	"github.com/snapas/img/iccjpeg"
+	"image"
+	"io"
+)
+
+type Image struct {
+	buf   *bytes.Buffer
+	Image image.Image
+	App2  []byte
+}
+
+func Decode(r io.Reader) (Image, string, error) {
+	i := Image{
+		buf: &bytes.Buffer{},
+	}
+	var err error
+
+	// Parse out needed metadata we need to retain
+	tr := io.TeeReader(r, i.buf)
+	i.App2, err = iccjpeg.GetICCRaw(tr)
+	if err != nil {
+		return i, "", fmt.Errorf("GetICCRaw: %s", err)
+	}
+
+	// Fix orientation
+	ri, s, err := imageorient.Decode(io.MultiReader(i.buf, r))
+	if err != nil {
+		return i, "", fmt.Errorf("imageorient.Decode: %s", err)
+	}
+
+	i.Image = ri
+	return i, s, nil
+}
+
+func (i Image) Bytes() []byte {
+	return i.buf.Bytes()
+}
+
+func (i Image) Write(p []byte) (n int, err error) {
+	n, err = i.buf.Write(p)
+	return
+}
