7e67ffc52d094eaac8f72552790b9bccc8c64932
Matt Baer <matt@write.as>
2021-06-11 08:39:05 -0400

    Add GetICCRaw test

---
+++ iccjpeg/iccjpeg_test.go
@@ -0,0 +1,21 @@
+package iccjpeg
+
+import (
+	"log"
+	"os"
+	"testing"
+)
+
+func TestGetICCRaw(t *testing.T) {
+	f, err := os.Open("../testdata/example.jpg")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	icc, err := GetICCRaw(f)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("%s", icc)
+}
